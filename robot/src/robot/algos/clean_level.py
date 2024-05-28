a202921a01167e819b9e3e8b7371721aaec203f9
Andrii Nikolaienko <anikolaienko14@gmail.com>
2024-05-28 12:35:10 +0100

    simpified the algorithm

--- robot/src/robot/algos/clean_level.py
+++ robot/src/robot/algos/clean_level.py
@@ -1,14 +1,14 @@
-from typing import Tuple, Callable, Awaitable
+from typing import Callable, Awaitable
 from heapq import heappush, heappop
 
 from robot.models.types import LevelType
 from robot.algos.clean_level_configs import get_directions
 
-LevelPositionType = Tuple[int, int]
+Position = tuple[int, int]
 LevelUpdateFunc = Callable[[list[list[str]]], Awaitable[bool]]
 
 
-def _find_start(level: LevelType) -> LevelPositionType:
+def _find_start(level: LevelType) -> Position:
     for row_idx in range(len(level)):
         row = level[row_idx]
         for col_idx in range(len(row)):
@@ -24,55 +24,34 @@ def _within_level(level: LevelType, row: int, col: int):
     return row >= 0 and row < rows and col >= 0 and col < cols
 
 
-def _find_next_near_position(
+def _find_route_to_next_position(
         level: LevelType,
-        curr_pos: LevelPositionType,
-        directions: list[tuple[int, int]]
-    ) -> LevelPositionType:
-    row, col = curr_pos
-
-    for row_delta, col_delta in directions:
-        row1, col1 = row + row_delta, col + col_delta
-        
-        if _within_level(level, row1, col1) and level[row1][col1] == "-":
-            return row1, col1
-    
-    return None
-
-def _find_route_to_not_visited_position(
-        level: LevelType,
-        curr_pos: LevelPositionType,
-        directions: list[tuple[int, int]]
-    ) -> list[LevelPositionType]:
-    visited = set()
-    parent = dict()
+        curr_pos: Position,
+        directions: list[tuple[int, int]],
+    ) -> list[Position]:
+    parents = {curr_pos: curr_pos}
     queue = [(0, curr_pos)]
 
     while queue:
         score, pos = heappop(queue)
-        if pos in visited:
-            continue
-        
+
         row, col = pos
         for row_delta, col_delta in directions:
-            row1, col1 = row + row_delta, col + col_delta
-            new_pos = (row1, col1)
+            new_row, new_col = row + row_delta, col + col_delta
+            new_pos = (new_row, new_col)
 
-            if _within_level(level, row1, col1) and new_pos not in visited:
-                if level[row1][col1] == "-":
-                    # build route and return
+            if _within_level(level, new_row, new_col) and new_pos not in parents:
+                if level[new_row][new_col] == "-":    # good position, build and return the route 
                     route = [new_pos]
-                    while pos in parent:
+                    while pos != parents[pos]:        # until position parent is position itself: start position
                         route.append(pos)
-                        pos = parent[pos]
+                        pos = parents[pos]
 
                     return reversed(route)
                 
-                if level[row1][col1] == " ":
-                    parent[new_pos] = pos
+                if level[new_row][new_col] == " ":    # already cleaned position, continue searching
+                    parents[new_pos] = pos
                     heappush(queue, (score + 1, new_pos))
-
-        visited.add(pos)
 
     return []
 
@@ -82,24 +61,14 @@ async def clean_level(name: str, level: LevelType, update_level: LevelUpdateFunc
     directions = get_directions(name)
 
     while True:
-        next_pos = _find_next_near_position(level, curr_pos, directions)
+        route = _find_route_to_next_position(level, curr_pos, directions)
+        if not route:
+            return
 
-        if next_pos is not None:
+        for next_pos in route:
             level[curr_pos[0]][curr_pos[1]] = " "
             level[next_pos[0]][next_pos[1]] = "R"
             curr_pos = next_pos
 
             if not await update_level(level):
                 return
-        else:
-            route = _find_route_to_not_visited_position(level, curr_pos, directions)
-            if not route:
-                return
-
-            for next_pos in route:
-                level[curr_pos[0]][curr_pos[1]] = " "
-                level[next_pos[0]][next_pos[1]] = "R"
-                curr_pos = next_pos
-
-                if not await update_level(level):
-                    return
